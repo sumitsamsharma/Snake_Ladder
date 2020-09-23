9ff96f542e8cdb2e5273319beb0c99827790b285
sumitsamsharma <sharmasumit94286@gmail.com>
2020-09-23 10:09:42 +0530

    UC 2

--- Game.java
+++ Game.java
@@ -5,7 +5,8 @@ public class Game
 	public static void main(String[] args)
 	{
 		int pos=0;
-		System.out.println("Player at position: "+pos);
+		int  dice_no=(int)Math.floor(Math.random()*100)%7;
+		System.out.println("Dice rolled a "+dice_no);	
 	}
 	
 }
